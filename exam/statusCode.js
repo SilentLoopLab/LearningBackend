3cf6c616a93d3b24c13a0a753067b00279b8700d
YeghyanAlen2004 <alenyeghyan74@gmail.com>
2025-12-23 17:59:08 +0400

    Ready :(

---
+++ exam/statusCode.js
@@ -0,0 +1,11 @@
+const statusCode = {
+    Ok: 200,
+    Created: 201,
+    No_Content: 204,
+    Bad_Request: 400,
+    Not_Found: 404,
+    Method_Not_Allowed: 405,
+    Internal_Server_Error: 500,
+};
+
+module.exports = statusCode;
